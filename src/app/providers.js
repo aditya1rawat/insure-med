158e8aaf6512b09c6b012edcab6e6ba2e6e98d2c
Aditya Rawat <aditya1rawat@gmail.com>
2023-05-06 11:36:40 -0700

    fixed css loading webpack and routing

--- src/app/providers.js
+++ src/app/providers.js
@@ -1,7 +1,17 @@
 'use client';
 
+import { useState } from 'react';
+
 import { ChakraProvider } from '@chakra-ui/react';
 
+import { createBrowserSupabaseClient } from '@supabase/auth-helpers-nextjs';
+import { SessionContextProvider } from '@supabase/auth-helpers-react';
+
 export function Providers({ children }) {
-	return <ChakraProvider>{children}</ChakraProvider>;
+	const [supabase] = useState(() => createBrowserSupabaseClient());
+	return (
+		<SessionContextProvider supabaseClient={supabase}>
+			<ChakraProvider>{children}</ChakraProvider>
+		</SessionContextProvider>
+	);
 }
