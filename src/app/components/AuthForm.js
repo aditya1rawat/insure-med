302b3c00bab7c8f5232ec96dbab8f94ed7168ef3
Aditya Rawat <aditya1rawat@gmail.com>
2023-05-06 13:32:03 -0700

    appearance

--- src/app/components/AuthForm.js
+++ src/app/components/AuthForm.js
@@ -1,9 +1,14 @@
 import { Auth, Typography, Button } from '@supabase/auth-ui-react';
+import { ThemeSupa } from '@supabase/auth-ui-shared';
 
 export default function AuthBasic({ supabase }) {
 	return (
 		<Auth.UserContextProvider supabaseClient={supabase}>
-			<Auth supabaseClient={supabase} providers={[]} />
+			<Auth
+				supabaseClient={supabase}
+				providers={[]}
+				appearance={{ theme: ThemeSupa }}
+			/>
 		</Auth.UserContextProvider>
 	);
 }
