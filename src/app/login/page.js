f71c66019bbc90467a1ec623084a08f52fda0553
Aditya Rawat <aditya1rawat@gmail.com>
2023-05-06 13:21:16 -0700

    auth

--- src/app/login/page.js
+++ src/app/login/page.js
@@ -5,18 +5,34 @@ import React from 'react';
 import { Auth } from '@supabase/auth-ui-react';
 import { useSession, useSupabaseClient } from '@supabase/auth-helpers-react';
 import Account from '../components/account';
+import { ThemeSupa } from '@supabase/auth-ui-shared';
+
+import AuthForm from '../components/authForm';
 
 export default function Page() {
 	const session = useSession();
 	const supabase = useSupabaseClient();
 	return (
 		<div>
-			<div className='container' style={{ padding: '50px 0 100px 0' }}>
-				{!session ? (
-					<Auth supabaseClient={supabase} />
-				) : (
-					<Account session={session} />
-				)}
+			<div
+				className='container'
+				style={{
+					padding: '50px 0 100px 0',
+					display: 'flex',
+					justifyContent: 'center'
+				}}
+			>
+				<div style={{ width: '500px' }}>
+					{!session ? (
+						// <Auth supabaseClient={supabase} />
+						<Auth
+							supabaseClient={supabase}
+							appearance={{ theme: ThemeSupa }}
+						/>
+					) : (
+						<Account session={session} />
+					)}
+				</div>
 			</div>
 		</div>
 	);
