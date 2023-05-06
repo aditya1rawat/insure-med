94895a0f34b2015e2cfbb836bc5e6e649241e279
BadrishAnanth <98199783+BadrishAnanth@users.noreply.github.com>
2023-05-06 15:44:39 -0700

    Background gradient

--- src/app/landing/page.js
+++ src/app/landing/page.js
@@ -5,7 +5,8 @@ export default function page() {
   return <div>
     <body>
     <button class = "button">Sign Up/Log In</button>
-    <header>Your Healthcare Simplified</header>
+    <img src="BackgroundGradient.png"></img>
+    <header class = "subheadline">Your Healthcare Simplified</header>
     <title>Say goodbye to those big medical bills!</title>
     <p>we cater to all these insurance providers: aksjdkfgdngiods</p>
     <p>Our Mission: xyzabc</p>
