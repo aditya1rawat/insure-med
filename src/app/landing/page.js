e2105eb8842e2fcda54b8e040f665cda46055f58
BadrishAnanth <98199783+BadrishAnanth@users.noreply.github.com>
2023-05-06 14:17:47 -0700

    Landing Page Update

--- src/app/landing/page.js
+++ src/app/landing/page.js
@@ -1,5 +1,11 @@
 import React from "react";
 
 export default function page() {
-  return <div>page</div>;
+  return <div>page
+    <header>Your Healthcare Simplified</header>
+    <title>Say goodbye to those big medical bills!</title>
+    <p>we cater to all these insurance providers: aksjdkfgdngiods</p>
+    <p>Our Mission: xyzabc</p>
+    <p>Interested? Sign up <href>here</href></p>
+      </div>;
 }
