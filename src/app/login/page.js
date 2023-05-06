8bd7a90431c9a15292cceff1002e4cddb4cb1c45
Aditya Rawat <aditya1rawat@gmail.com>
2023-05-06 12:26:40 -0700

    authentication

--- src/app/login/page.js
+++ src/app/login/page.js
@@ -1,5 +1,23 @@
+'use client';
+
 import React from 'react';
 
+import { Auth } from '@supabase/auth-ui-react';
+import { useSession, useSupabaseClient } from '@supabase/auth-helpers-react';
+import Account from '../components/account';
+
 export default function Page() {
-	return <div>login</div>;
+	const session = useSession();
+	const supabase = useSupabaseClient();
+	return (
+		<div>
+			<div className='container' style={{ padding: '50px 0 100px 0' }}>
+				{!session ? (
+					<Auth supabaseClient={supabase} />
+				) : (
+					<Account session={session} />
+				)}
+			</div>
+		</div>
+	);
 }
