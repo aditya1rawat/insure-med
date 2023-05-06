6e3785952908ee11c116e465ad22b3c900de51ae
BadrishAnanth <98199783+BadrishAnanth@users.noreply.github.com>
2023-05-06 15:23:25 -0700

    Added CSS + Landing Page

--- src/app/landing/page.js
+++ src/app/landing/page.js
@@ -1,11 +1,15 @@
 import React from "react";
+import "../globals.css"
 
 export default function page() {
-  return <div>page
+  return <div>
+    <body>
+    <button class = "button">Sign Up/Log In</button>
     <header>Your Healthcare Simplified</header>
     <title>Say goodbye to those big medical bills!</title>
     <p>we cater to all these insurance providers: aksjdkfgdngiods</p>
     <p>Our Mission: xyzabc</p>
     <p>Interested? Sign up <href>here</href></p>
+    </body>
       </div>;
 }
