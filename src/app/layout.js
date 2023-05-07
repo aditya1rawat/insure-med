780cd0ab008b1806f132ea98de7b4db10cab8704
Aditya Rawat <aditya1rawat@gmail.com>
2023-05-07 13:37:44 -0700

    landing fixes

--- src/app/layout.js
+++ src/app/layout.js
@@ -11,6 +11,7 @@ export default function RootLayout({ children }) {
 	return (
 		<>
 			<Providers>
+				<Navbar />
 				{children}
 				<Footer />
 			</Providers>
--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,9 @@
 'use client';
 import {
+	Box,
 	Button,
 	Flex,
+	HStack,
 	Heading,
 	Image,
 	Link,
@@ -16,93 +18,60 @@ import { BsSearch } from 'react-icons/bs';
 export default function SplitScreen() {
 	return (
 		<>
-			<AuthNavbar />
-			{/* <Stack
-				minH={'100vh'}
+			<Box
 				direction={{ base: 'column', md: 'row' }}
 				style={{
-					background: 'url(/BackgroundGradient.png)'
+					background:
+						'linear-gradient(175deg, #b92b27, #A600EA, #6DD5FA 70%, #FFF calc(75%))'
 				}}
+				height={'49vh'}
 			>
-				<Flex p={8} flex={1} align={'center'} justify={'center'}>
-					<Stack spacing={6} w={'full'} maxW={'lg'}>
-						<Heading
-							fontSize={{ base: '3xl', md: '4xl', lg: '5xl' }}
-						>
-							<Text
-								as={'span'}
-								position={'relative'}
-								_after={{
-									content: "''",
-									width: 'full',
-									height: useBreakpointValue({
-										base: '20%',
-										md: '30%'
-									}),
-									position: 'absolute',
-									bottom: 1,
-									left: 0,
-									bg: 'blue.400',
-									zIndex: -1
-								}}
-							>
-								Your Healthcare
-							</Text>
-							<br />{' '}
-							<Text color={'blue.400'} as={'span'}>
-								Simplified
-							</Text>{' '}
-						</Heading>
-						<Text
-							fontSize={{ base: 'md', lg: 'lg' }}
-							color={'gray.500'}
-						>
-							Say goodbye to those big medical bills!
-						</Text>
-						<Stack
-							direction={{ base: 'column', md: 'row' }}
-							spacing={4}
-						>
+				<Box
+					display={'flex'}
+					justifyContent={'space-between'}
+					mx='48'
+					py={'56'}
+					alignItems={'center'}
+				>
+					<Box>
+						<Box opacity={'65%'}>
+							<Heading fontSize={'90px'}>
+								Your Healthcare,
+							</Heading>
+							<Heading fontSize={'70px'}>
+								<u>Simplified</u>
+							</Heading>
+						</Box>
+
+						<HStack spacing={'5'} my={'10'}>
 							<Button
-								rounded={'full'}
-								bg={'blue.400'}
-								color={'white'}
-								_hover={{
-									bg: 'blue.500'
-								}}
+								as={'a'}
+								href='/add'
 								leftIcon={<BsSearch />}
+								bgColor={'black'}
+								colorScheme='teal'
+								variant='solid'
+								size={'md'}
+								_hover={{
+									backgrouColor: 'black'
+								}}
 							>
 								Search
 							</Button>
-						</Stack>
-					</Stack>
-				</Flex>
-				<Flex flex={1}>
-					<Image
-						alt={'Login Image'}
-						objectFit={'cover'}
-						src={
-							'https://img.freepik.com/premium-photo/close-up-stethoscope-computer-keyboard-white-desk-online-health-care-telemedicine-concept-medical-background_136875-2032.jpg?w=2000'
-						}
-					/>
-				</Flex>
-			</Stack> */}
-			<Link href='/add'>
-				<Image
-					alt={'Landing'}
-					objectFit={'cover'}
-					width={'100%'}
-					src={'/Home.svg'}
-				/>
-			</Link>
-			<Link href='/signup'>
-				<Image
-					alt={'Landing'}
-					objectFit={'cover'}
-					width={'100%'}
-					src={'/register.svg'}
-				/>
-			</Link>
+							<Button
+								as={'a'}
+								variant='outline'
+								size={'md'}
+								href='/signup'
+							>
+								Register
+							</Button>
+						</HStack>
+					</Box>
+					<Image src='/appdemo.svg' width={'600px'} height={'auto'} />
+				</Box>
+			</Box>
+			<Box height={'37vh'}></Box>
 		</>
 	);
 }
