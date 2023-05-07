2e0692d1e4fc51d2dd5ef2369a74de0e87ce91e9
Aditya Rawat <aditya1rawat@gmail.com>
2023-05-06 17:18:26 -0700

    search functionality

--- src/app/add/page.js
+++ src/app/add/page.js
@@ -1,62 +1,80 @@
-"use client";
+'use client';
 
-import React, { useState } from "react";
+import React, { useState } from 'react';
 import {
-  Input,
-  Stack,
-  InputGroup,
-  InputLeftElement,
-  InputRightElement,
-  Box,
-} from "@chakra-ui/react";
-import { SearchIcon, CloseIcon } from "@chakra-ui/icons";
+	Input,
+	Stack,
+	InputGroup,
+	InputLeftElement,
+	InputRightElement,
+	Box,
+	VStack,
+	Button,
+	Link
+} from '@chakra-ui/react';
+import { SearchIcon, CloseIcon } from '@chakra-ui/icons';
 
-import Hospital from "../components/Hospital";
+import Hospital from '../components/Hospital';
+import AuthNavbar from '../components/AuthNavbar';
 
 export default function Page() {
-  const [searchTerm, setSearchTerm] = useState("");
+	const [searchTerm, setSearchTerm] = useState('');
 
-  return (
-    <Box
-      style={{
-        width: "60%",
-        margin: "auto",
-        justifyContent: "center",
-        display: "flex",
-        flexDirection: "column",
-      }}
-    >
-      <InputGroup
-        style={{ marginTop: 10, position: "sticky", zIndex: 10, top: 0 }}
-      >
-        <InputLeftElement pointerEvents={"none"}>
-          <SearchIcon color={"black"} />
-        </InputLeftElement>
-        <Input
-          type={"tel"}
-          placeholder={"Search for Procedures"}
-          style={{ borderRadius: 30 }}
-          value={searchTerm}
-          onChange={(e) => {
-            e.preventDefault();
-            setSearchTerm(e.target.value);
-          }}
-        />
-        <InputRightElement>
-          <CloseIcon
-            color={"black"}
-            onClick={(e) => {
-              e.preventDefault();
-              setSearchTerm("");
-            }}
-          />
-        </InputRightElement>
-      </InputGroup>
+	return (
+		<>
+			<AuthNavbar />
+			<Box
+				style={{
+					width: '60%',
+					margin: 'auto',
+					justifyContent: 'center',
+					display: 'flex',
+					flexDirection: 'column'
+				}}
+			>
+				<VStack>
+					<InputGroup
+						style={{
+							marginTop: 10,
+							position: 'sticky',
+							zIndex: 10,
+							top: 0
+						}}
+					>
+						<InputLeftElement pointerEvents={'none'}>
+							<SearchIcon color={'black'} />
+						</InputLeftElement>
+						<Input
+							type={'tel'}
+							placeholder={'Search for Procedures'}
+							style={{ borderRadius: 30 }}
+							value={searchTerm}
+							onChange={e => {
+								e.preventDefault();
+								setSearchTerm(e.target.value);
+								console.log(searchTerm);
+							}}
+						/>
+						<InputRightElement>
+							<CloseIcon
+								color={'black'}
+								onClick={e => {
+									e.preventDefault();
+									setSearchTerm('');
+								}}
+							/>
+						</InputRightElement>
+					</InputGroup>
+					<Link href='/add'>
+						<Button>Search</Button>
+					</Link>
+				</VStack>
 
-      <Hospital />
-      <Hospital />
-      <Hospital />
-      <Hospital />
-    </Box>
-  );
+				<Hospital />
+				<Hospital />
+				<Hospital />
+				<Hospital />
+			</Box>
+		</>
+	);
 }
